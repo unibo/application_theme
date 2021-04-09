13e8ff9fe97e2c764e7c9d7e6a39e9d9b130449a
stefania scolamiero <stefania.scolamiero@abstract.it>
2021-04-09 18:47:16 +0200

    upgrade foundation

---
+++ src/assets/js/app.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+import 'what-input';
+
+
+// Foundation JS relies on a global variable. In ES6, all imports are hoisted
+// to the top of the file so if we used `import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+window.jQuery = $;
+require('foundation-sites');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+// Original js
+// $(document).foundation();
+
+import './font-awesome';
+import './slick.min';
+import './u_to_top';
+
+$(document).on('ready page:load', function () {
+    $(document).foundation();
+});
+
