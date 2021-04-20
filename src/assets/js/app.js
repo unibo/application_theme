5ec2b2b9786c000dee62be2de03e770990de660a
stefania scolamiero <stefania.scolamiero@abstract.it>
2021-04-20 14:34:55 +0200

    change js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,30 +1,34 @@
+// import jQuery
 import $ from 'jquery';
+import 'foundation-sites';
 import 'what-input';
 import hljs from 'highlight.js';
 
 window.hljs = hljs;
 
 // Foundation JS relies on a global variable. In ES6, all imports are hoisted
-// to the top of the file so if we used `import` to import Foundation,
+// to the top of the file so if we used import to import Foundation,
 // it would execute earlier than we have assigned the global variable.
 // This is why we have to use CommonJS require() here since it doesn't
 // have the hoisting behavior.
 window.jQuery = $;
+// require('foundation-sites');
 require('foundation-sites');
 
-
 // If you want to pick and choose which modules to include, comment out the above and uncomment
-// the line below
-//import './lib/foundation-explicit-pieces';
+// // the line below
+// import './lib/foundation-explicit-pieces';
 
 // Original js
 // $(document).foundation();
+
 
 import './font-awesome';
 import './slick.min';
 import './u_to_top';
 import './toc';
 
-$(document).on('ready page:load', function () {
+
+//window.onload = function () {
     $(document).foundation();
-});
+//};
